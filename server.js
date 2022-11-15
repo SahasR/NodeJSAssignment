046d08cb8714690fab85b5be4b7b65da84833372
sahas <sahasrenuja@gmail.com>
2022-11-15 11:05:46 +0530

    Added Cors

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import task_routes from './routes/tasks.js';
+import cors from 'cors';
 
 const app = express();
 const jsonParser = bodyParser.json();
 
-app.listen(3000, function() {
-    console.log('Listening on http://localhost:3000/')
+app.listen(3001, function() {
+    console.log('Listening on http://localhost:3001/')
 })
 
 //Another piece of middleware we want to run to get the bodys to parse.
+app.use(cors());
 app.use(jsonParser);
 app.use('/', task_routes);
-
-
-
-
