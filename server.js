c589221ef8a88ee675933d59a1dc327d10330806
sahas <sahasrenuja@gmail.com>
2022-11-15 11:37:49 +0530

    Implement cors functionality

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ import cors from 'cors';
 const app = express();
 const jsonParser = bodyParser.json();
 
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+
 app.listen(3001, function() {
     console.log('Listening on http://localhost:3001/')
 })
 
 //Another piece of middleware we want to run to get the bodys to parse.
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(jsonParser);
 app.use('/', task_routes);
